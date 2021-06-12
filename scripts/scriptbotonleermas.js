c5ffc706aef32cc255bfb807dc7a3915857425d5
jimenavargas <you@example.com>
2021-06-12 00:05:42 -0600

    Proyecto version final

--- scripts/scriptbotonleermas.js
+++ scripts/scriptbotonleermas.js
@@ -7,7 +7,7 @@ function mostrarMas(){
         masTexto.style.display="block"
 
     }else{
-        boton.textContent = "Leer mas"
+        boton.textContent = "Leer más"
        masTexto.style.display="none"
     }
 
@@ -16,7 +16,15 @@ function toggleHobbies(){
     var mascotas_galeria=document.getElementById("mascotas_galeria"); 
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var paseos_galeria=document.getElementById("paseos_galeria"); 
-    
+   
+    var botonHobbies=document.getElementById("botonHobbies");
+    var botonMascotas=document.getElementById("botonMascotas");
+    var botonPaseos=document.getElementById("botonPaseos");
+
+    botonHobbies.style.background="lightsalmon";
+    botonMascotas.style.background="lightseagreen";
+    botonPaseos.style.background="lightseagreen";
+
     paseos_galeria.style.display="none";
     hobbies_galeria.style.display="block";
     mascotas_galeria.style.display="none";
@@ -27,6 +35,14 @@ function toggleMascotas(){
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var paseos_galeria=document.getElementById("paseos_galeria"); 
   
+    var botonHobbies=document.getElementById("botonHobbies");
+    var botonMascotas=document.getElementById("botonMascotas");
+    var botonPaseos=document.getElementById("botonPaseos");
+
+    botonHobbies.style.background="lightseagreen";
+    botonMascotas.style.background="lightsalmon";
+    botonPaseos.style.background="lightseagreen";
+    
     paseos_galeria.style.display="none";
     hobbies_galeria.style.display="none";
     mascotas_galeria.style.display="block";
@@ -38,6 +54,14 @@ function togglePaseos(){
     var hobbies_galeria=document.getElementById("hobbies_galeria"); 
     var paseos_galeria=document.getElementById("paseos_galeria"); 
     
+    var botonHobbies=document.getElementById("botonHobbies");
+    var botonMascotas=document.getElementById("botonMascotas");
+    var botonPaseos=document.getElementById("botonPaseos");
+
+    botonHobbies.style.background="lightseagreen";
+    botonMascotas.style.background="lightseagreen";
+    botonPaseos.style.background="lightsalmon";
+   
     paseos_galeria.style.display="block";
     hobbies_galeria.style.display="none";
     mascotas_galeria.style.display="none";
